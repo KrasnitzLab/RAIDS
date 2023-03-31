da6e6d63ab8b85a127c347094162ecd8e1fd04ab
Astrid Deschenes <adeschen@hotmail.com>
2023-03-30 22:05:51 -0400

    Move getTableSNV() to internal and remove internal validations

--- tests/testthat/test-allelicFraction_internal.R
+++ tests/testthat/test-allelicFraction_internal.R
@@ -4,24 +4,3 @@ library(RAIDS)
 library(withr)
 library(gdsfmt)
 
-
-#############################################################################
-### Tests validateGetTableSNV() results
-#############################################################################
-
-context("validateGetTableSNV() results")
-
-test_that("validateGetTableSNV() must return expected results when all input are valid", {
-
-    data.dir <- test_path("fixtures")
-    gdsFIle <- file.path(data.dir, "1KG_Test.gds")
-
-    gdsF <- openfn.gds(gdsFIle)
-    withr::defer((gdsfmt::closefn.gds(gdsF)), envir = parent.frame())
-
-    result1 <- RAIDS:::validateGetTableSNV(gdsRef=gdsF, gdsSample=gdsF,
-                    sampleCurrent="TEST", study.id="TEST",
-                    minCov=10L, minProb=0.999, eProb=0.001,verbose=TRUE)
-
-    expect_identical(result1, 0L)
-})

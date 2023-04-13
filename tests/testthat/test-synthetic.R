d8cee31a392794ffb6be207ec06eae373ff3fa0a
belleau <belleau@belleauMBP.home>
2023-04-13 15:40:29 -0400

    Change variables name

--- tests/testthat/test-synthetic.R
+++ tests/testthat/test-synthetic.R
@@ -13,12 +13,12 @@ library(testthat)
 context("select1KGPop() results")
 
 
-test_that("select1KGPop() must return error when gds is a character string", {
+test_that("select1KGPop() must return error when gdsReference is a character string", {
 
-    error_message <- paste0("The \'gds\' must be an object of class ",
+    error_message <- paste0("The \'gdsReference\' must be an object of class ",
                                 "\'gds.class\'")
 
-    expect_error(select1KGPop(gds="test.gds", nbSamples=10L),
+    expect_error(select1KGPop(gdsReference="test.gds", nbSamples=10L),
                     error_message)
 })
 
@@ -35,7 +35,7 @@ test_that("select1KGPop() must return error when nbSamples is a character string
     error_message <- paste0("The \'nbSamples\' parameter must be a ",
                                 "single positive integer.")
 
-    expect_error(select1KGPop(gds=gdsF, nbSamples="CANADA"),
+    expect_error(select1KGPop(gdsReference=gdsF, nbSamples="CANADA"),
                     error_message)
 })
 
@@ -55,7 +55,7 @@ test_that("select1KGPop() must return expected result", {
                             superPop=rep("EUR", 4))
 
     set.seed(1212)
-    results <- select1KGPop(gds=gdsF, nbSamples=4L)
+    results <- select1KGPop(gdsReference=gdsF, nbSamples=4L)
 
     expect_equal(results, expected)
 })

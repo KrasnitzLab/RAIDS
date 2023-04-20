e3b0c104be86407e0955e75f36b5aa5399a6c651
Astrid Deschenes <adeschen@hotmail.com>
2023-04-19 20:56:44 -0400

    Move internal functions to internal files; add example to prepSynthetic() doc

--- tests/testthat/test-allelicFraction.R
+++ tests/testthat/test-allelicFraction.R
@@ -87,21 +87,412 @@ test_that("computeAlleleFraction() must return expected results with small w par
                                 nrow=1))
 })
 
+
 #############################################################################
-### Tests computeAlleleFraction() results
+### Tests estimateAllelicFraction() results
 #############################################################################
 
-context("computeLOHBlocksDNAChr() results")
+
+context("estimateAllelicFraction() results")
 
 
-test_that("computeLOHBlocksDNAChr() must return expected results", {
+test_that("estimateAllelicFraction() must return error when gdsReference is a character string", {
 
-    dataDir <- testthat::test_path("fixtures")
-    fileGDS <- file.path(dataDir, "ex1_good_small_1KG_GDS.gds")
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
 
-    ## Open the reference GDS file
-    gds1KG <- snpgdsOpen(fileGDS)
-    withr::defer((gdsfmt::closefn.gds(gds1KG)), envir = parent.frame())
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- "The \'gdsReference\' must be an object of class \'gds.class\'."
+
+    expect_error(estimateAllelicFraction(gdsReference="test.gds",
+        gdsProfile=gdsF, currentProfile="gds", studyID= "TCGA",
+        chrInfo=chrInfo, studyType="DNA", minCov=10, minProb=0.999,
+        eProb=0.001, cutOffLOH=-5, cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when gdsSample is a character string", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- "The \'gdsProfile\' must be an object of class \'gds.class\'."
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile="test.gds", currentProfile="gds", studyID= "TCGA",
+        chrInfo=chrInfo, studyType="DNA", minCov=10, minProb=0.999,
+        eProb=0.001, cutOffLOH=-5, cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when currentProfile is a numeric value", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- "The \'currentProfile\' must be a character string."
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile=33, studyID= "TCGA",
+        chrInfo=chrInfo, studyType="DNA", minCov=10, minProb=0.999,
+        eProb=0.001, cutOffLOH=-5, cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when studyID is a numeric value", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- "The \'studyID\' must be a character string."
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID= 33,
+        chrInfo=chrInfo, studyType="DNA", minCov=10, minProb=0.999,
+        eProb=0.001, cutOffLOH=-5, cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when studyType is a numeric value", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- "The \'studyType\' must be a character string."
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType=33, minCov=10, minProb=0.999,
+        eProb=0.001, cutOffLOH=-5, cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when minCov is vector of numeric values", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- "The \'minCov\' must be a single numeric positive value"
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType="DNA", minCov=c(10, 11), minProb=0.999,
+        eProb=0.001, cutOffLOH=-5, cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when minCov is character string", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- "The \'minCov\' must be a single numeric positive value"
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType="DNA", minCov="3", minProb=0.999,
+        eProb=0.001, cutOffLOH=-5, cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when minProb is character string", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- paste0("The \'minProb\' must be a single numeric positive ",
+                                "value between 0 and 1.")
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType="DNA", minCov=10L, minProb="0.1",
+        eProb=0.001, cutOffLOH=-5, cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when minProb is negative value", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- paste0("The \'minProb\' must be a single numeric positive ",
+                                "value between 0 and 1.")
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType="DNA", minCov=10L, minProb=-0.1,
+        eProb=0.001, cutOffLOH=-5, cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when minProb is above 1", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- paste0("The \'minProb\' must be a single numeric positive ",
+                                "value between 0 and 1.")
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType="DNA", minCov=10L, minProb=1.01,
+        eProb=0.001, cutOffLOH=-5, cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when minProb is vector of numeric values", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- paste0("The \'minProb\' must be a single numeric positive ",
+                                "value between 0 and 1.")
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType="DNA", minCov=10L, minProb=c(0.01, 0.02),
+        eProb=0.001, cutOffLOH=-5, cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when eProb is character string", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- paste0("The \'eProb\' must be a single numeric positive ",
+                                "value between 0 and 1.")
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType="DNA", minCov=10L, minProb=0.1,
+        eProb="0.001", cutOffLOH=-5, cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when eProb is negative value", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- paste0("The \'eProb\' must be a single numeric positive ",
+                                "value between 0 and 1.")
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType="DNA", minCov=10L, minProb=0.1,
+        eProb=-0.01, cutOffLOH=-5, cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when eProb is above 1", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- paste0("The \'eProb\' must be a single numeric positive ",
+                                "value between 0 and 1.")
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType="DNA", minCov=10L, minProb=0.1,
+        eProb=1.01, cutOffLOH=-5, cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when eProb is vector of numeric values", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- paste0("The \'eProb\' must be a single numeric positive ",
+                                "value between 0 and 1.")
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType="DNA", minCov=10L, minProb=0.1,
+        eProb=c(0.01, 0.001), cutOffLOH=-5, cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when wAR is vector of numeric values", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- "The \'wAR\' parameter must be a single numeric positive value."
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType="DNA", minCov=10L, minProb=0.1,
+        eProb=0.01, cutOffLOH=-5, cutOffHomoScore=-3,
+        wAR="9"), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when cutOffLOH is vector of numeric values", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- "The \'cutOffLOH\' parameter must be a single numeric value."
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType="DNA", minCov=10L, minProb=0.1,
+        eProb=0.01, cutOffLOH=c(-5, -3), cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when cutOffLOH is a character string", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- "The \'cutOffLOH\' parameter must be a single numeric value."
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType="DNA", minCov=10L, minProb=0.1,
+        eProb=0.01, cutOffLOH="-3", cutOffHomoScore=-3,
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+
+
+test_that("estimateAllelicFraction() must return error when cutOffLOH is vector of numeric values", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- "The \'cutOffHomoScore\' parameter must be a single numeric value."
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType="DNA", minCov=10L, minProb=0.1,
+        eProb=0.01, cutOffLOH=-5, cutOffHomoScore=c(-3, -4),
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when cutOffHomoScore is a character string", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- "The \'cutOffHomoScore\' parameter must be a single numeric value."
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType="DNA", minCov=10L, minProb=0.1,
+        eProb=0.01, cutOffLOH=-5, cutOffHomoScore="-3",
+        wAR=9), error_message, fixed=TRUE)
+})
+
+
+test_that("estimateAllelicFraction() must return error when verbose is not a numeric", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    error_message <- "The \'verbose\' parameter must be a logical (TRUE or FALSE)."
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA",
+        chrInfo=chrInfo, studyType="TOTO", minCov=10L, minProb=0.1,
+        eProb=0.01, cutOffLOH=-5, cutOffHomoScore=-3, wAR=9, verbose=22),
+        error_message, fixed=TRUE)
+})
+
+test_that("estimateAllelicFraction() must return error when studyType is not a valid choice", {
+
+    fileGDS <- test_path("fixtures", "1KG_Test.gds")
+
+    gdsF <- openfn.gds(fileGDS)
+    withr::defer((gdsfmt::closefn.gds(gdsF)), envir=parent.frame())
+
+    expect_error(estimateAllelicFraction(gdsReference=gdsF,
+        gdsProfile=gdsF, currentProfile="test", studyID="DNA_1",
+        chrInfo=chrInfo, studyType="TOTO", minCov=10L, minProb=0.1,
+        eProb=0.01, cutOffLOH=-5, cutOffHomoScore=-3, wAR=9))
+})
+
+
+test_that("estimateAllelicFraction() must return valid results", {
+
+    file1KG <- test_path("fixtures", "ex1_good_small_1KG_GDS.gds")
+
+    gds1KG <- openfn.gds(file1KG)
+    withr::defer((gdsfmt::closefn.gds(gds1KG)), envir=parent.frame())
+
+
+    dataDirSample <- test_path("fixtures/sampleGDSforEstimAlleFraction")
+    file.copy(file.path(dataDirSample, "ex1_demoForEstimAllFrac.gds"),
+                file.path(dataDirSample, "ex1.gds"))
+    withr::defer((unlink(file.path(dataDirSample, "ex1.gds"))),
+                    envir=parent.frame())
+
+    gdsProfile <- openfn.gds(file.path(dataDirSample, "ex1.gds"),
+                                readonly=FALSE)
+    withr::defer((gdsfmt::closefn.gds(gdsProfile)), envir=parent.frame())
 
     chrInfo <- c(248956422L, 242193529L, 198295559L, 190214555L,
         181538259L, 170805979L, 159345973L, 145138636L, 138394717L, 133797422L,
@@ -109,41 +500,20 @@ test_that("computeLOHBlocksDNAChr() must return expected results", {
         83257441L,  80373285L,  58617616L,  64444167L,  46709983L, 50818468L,
         156040895L, 57227415L,  16569L)
 
+    result <- estimateAllelicFraction(gdsReference=gds1KG,
+        gdsProfile=gdsProfile, currentProfile="ex1", studyID="MYDATA",
+        chrInfo=chrInfo, studyType="DNA", minCov=10L, minProb=0.999,
+        eProb=0.001, cutOffLOH=-5, cutOffHomoScore=-3, wAR=9, cutOffAR=3,
+        gdsRefAnnot=NULL, blockID=NULL, verbose=FALSE)
 
-    ## Data frame with SNV information for the specified chromosome (chr 1)
-    snpInfo <- data.frame(cnt.tot=c(41, 17, 27, 15, 11, 37, 29, 36, 26, 32, 29, 26),
-                    cnt.ref=c(40, 37, 37, 25,  4, 14, 26, 32, 26, 32, 34, 31),
-                    cnt.alt=c(1,   8,  6,  5, 12, 54,  3,  1,  0,  0,  2,  5),
-                    snp.pos=c(3722256, 3722328, 3767522, 3868160, 3869467, 4712655,
-                            6085318, 6213145, 6385318, 6313145, 6485318, 6413145),
-                    snp.chr=c(rep(1, 12)),
-                    normal.geno=c(rep(1, 12)),
-                    pruned=rep(TRUE, 12),
-                    snp.index=c(160, 162, 204, 256, 259, 288, 366,
-                                    465, 467, 477, 488, 489),
-                    keep=rep(TRUE, 12),
-                    hetero=c(rep(TRUE, 8), FALSE, TRUE, TRUE, TRUE),
-                    homo=c(rep(FALSE, 8), TRUE, FALSE, FALSE, FALSE),
-                    stringAsFactor=FALSE)
+    expect_equal(result, 0L)
 
-    result <- RAIDS:::computeLOHBlocksDNAChr(gdsReference=gds1KG,
-         chrInfo=chrInfo, snp.pos=snpInfo, chr=1L, genoN=0.0001)
+    result1 <- read.gdsn(index.gdsn(node=gdsProfile, path="lap"))
+    result2 <- read.gdsn(index.gdsn(node=gdsProfile, path="segment"))
 
-    expected <- data.frame(chr=rep(1, 12),
-                           start=c(1, snpInfo$snp.pos[seq_len(8)]+1, 6313146,
-                                        6485319, 6413146),
-                           end=c(snpInfo$snp.pos[seq_len(8)], 6313145,
-                                        6485318, 6413145, 248956422),
-                           logLHR=c(rep(0, 9), -100, 0, 0),
-                           LH1=c(rep(0, 9), -10.729445965503494, 0, 0),
-                           LM1=c(rep(0, 9), -0.912269425836972, 0, 0),
-                           homoScore=c(rep(0, 9), -9.817176539666523, 0, 0),
-                           nbSNV=c(rep(0, 9), 1, 0, 0),
-                           nbPruned=c(rep(0, 9), 1, 0, 0),
-                           nbNorm=c(rep(0, 9), 1, 0, 0),
-                           LOH=c(rep(0, 12)))
-
-    expect_equal(result, expected)
-
+    expect_equal(result1, rep(0.5, 61))
+    expect_equal(result2, c(rep(1, 6), 2, 3, rep(4, 5), rep(5, 4), rep(6, 3),
+        rep(7, 2), 9, rep(10, 5), rep(11, 4), 12, 12, rep(13, 4), rep(14, 4),
+        15, rep(16, 3), rep(17, 3), 18, 18, 19, 19, rep(20, 3), 21, rep(22, 4)))
 })
 

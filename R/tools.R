2a9bc94e41f7dc04bb0d25ff3dd200204630a789
belleau <belleau@belleauMBP.home>
2023-08-21 18:47:15 -0400

    Remove TOREVIEW LOOP_REMOVE

--- R/tools.R
+++ R/tools.R
@@ -205,27 +205,6 @@ groupChr1KGSNV <- function(pathGenoChr, pathOut) {
     listFiles <- dir(file.path(pathGenoChr, "chr1"), ".+\\.chr1\\.vcf\\.bz2")
     listSamples <- gsub("\\.chr1\\.vcf\\.bz2", "", listFiles)
 
-    ## TOREVIEW FOR_LOOP REMOVAL
-    ## Merge files associated to each samples into one csv file
-    # for(sampleId in listSamples) {
-    #
-    #     ## TOREVIEW FOR_LOOOP REMOVAL
-        ## Read each genotyping file and append the information
-        # listGeno <- list()
-        # for(chr in seq_len(22)) {
-        #     geno <- read.csv2(file.path(pathGenoChr, paste0("chr", chr),
-        #                           paste0(sampleId, ".chr", chr,".vcf.bz2")),
-        #                       sep="\t", row.names=NULL)
-        #
-        #     listGeno[[paste0("chr", chr)]] <- geno
-        # }
-        # genoAll <- do.call(rbind, listGeno)
-#
-#         ## Save the genotyping information into one file
-#         write.csv2(genoAll, file=bzfile(file.path(pathOut,
-#                             paste0(sampleId, ".csv.bz2"))), row.names=FALSE)
-#     }
-
 
     ## Merge files associated to each samples into one csv file
     results <- lapply(X=listSamples, FUN=function(sampleId, pathOut) {

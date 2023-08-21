e510f4e2a36f54dcd4b46c113ba9cfa5eb872e8b
belleau <belleau@belleauMBP.home>
2023-08-21 18:27:17 -0400

    Add examples and update manual page of addGeneBlockGDSRefAnnot generateGeneBlock

--- R/process1KG.R
+++ R/process1KG.R
@@ -718,8 +718,6 @@ addRef2GDS1KG <- function(fileNameGDS, filePart) {
     return(0L)
 }
 
-
-
 #' @title Extract the specified column from the 1KG GDS 'sample.ref' node
 #' for the reference profiles (real ancestry assignation)
 #'
@@ -819,10 +817,46 @@ getRef1KGPop <- function(gdsReference, popName="superPop") {
 #'
 #' @examples
 #'
+#' ## Required library
+#' library(SNPRelate)
+#'
 #' ## Path to the demo pedigree file is located in this package
 #' dataDir <- system.file("extdata", package="RAIDS")
 #'
-#' ## TODO
+#'
+#' fileAnnotGDS <- file.path(getwd(), "ex1_good_small_1KG_Annot_GDS.gds")
+#' ## Required library
+#' if (file.access(getwd()) == 0 && !file.exists(fileAnnotGDS)) {
+#'
+#'     if (requireNamespace("EnsDb.Hsapiens.v86", quietly=TRUE)) {
+#'
+#'        file.copy(file.path(dataDir, "tests", "ex1_NoBlockGene.1KG_Annot_GDS.gds"),
+#'                  fileAnnotGDS)
+#'         ## Making a "short cut" on the ensDb object
+#'         edb <- EnsDb.Hsapiens.v86::EnsDb.Hsapiens.v86
+#'
+#'
+#'         ## Temporary Profile GDS file for one profile
+#'         fileReferenceGDS  <- file.path(dataDir, "tests",
+#'                 "ex1_good_small_1KG_GDS.gds")
+#'
+#'         ## Open the reference GDS file (demo version)
+#'         gds1KG <- snpgdsOpen(fileReferenceGDS)
+#'
+#'
+#'         ## The function
+#'         addGeneBlockGDSRefAnnot(gdsReference=gds1KG,
+#'             gdsRefAnnotFile=fileAnnotGDS,
+#'             ensDb=edb,
+#'             suffixBlockName="EnsDb.Hsapiens.v86")
+#'         gdsAnnot1KG <- openfn.gds(fileAnnotGDS)
+#'         print(gdsAnnot1KG)
+#'         print(read.gdsn(index.gdsn(gdsAnnot1KG, "block.annot")))
+#'         closefn.gds(gds1KG)
+#'         closefn.gds(gdsAnnot1KG)
+#'         unlink(fileAnnotGDS, force=TRUE)
+#'     }
+#' }
 #'
 #' @author Pascal Belleau, Astrid Deschênes and Alexander Krasnitz
 #' @importFrom gdsfmt openfn.gds closefn.gds
